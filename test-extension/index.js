d258c3e0e5fbcec2fbf9aa3f831cbfcd0b04b825
matias acuna <agroang@gmail.com>
2022-02-10 09:58:36 +0900

    updated resources

--- test-extension/index.js
+++ test-extension/index.js
@@ -1,2 +1,11 @@
 // Doc from Chrome:
 // https://developer.chrome.com/docs/extensions/mv3/getstarted/
+
+// To check the extensions:
+// chrome://extensions/
+
+// For user location:
+// https://developers.google.com/web/fundamentals/native-hardware/user-location
+
+// Reading right now:
+// https://developer.chrome.com/docs/extensions/mv3/intro/mv3-overview/
